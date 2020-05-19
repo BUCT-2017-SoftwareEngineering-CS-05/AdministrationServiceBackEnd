9af248e2b6793fe8d7deda4e85a17af25c223b0b
Shi-hai <thisisid@163.com>
2020-05-19 21:42:32 +0800

    Signed-off-by: Shi-hai <thisisid@163.com>

--- Services/UserSystem.cs
+++ Services/UserSystem.cs
@@ -16,23 +16,21 @@ namespace MuseumBackend.Models
         
         public static bool Mute(User user)
         {
-            if (!_user.GetAllUsers().Contains(user))
+            if (_user.GetUserById(user.Userid) == null)
                 return false;
             _user.MuteUser(user.Userid, 0);
             return true;
         }
         public static bool CancelMute(User user)
         {
-            if (!_user.GetAllUsers().Contains(user))
+            if (_user.GetUserById(user.Userid) == null)
                 return false;
             _user.MuteUser(user.Userid, 0);
             return true;
         }
         public static bool DeleteOneComment(User user, int midex)
         {
-            if (!_user.GetAllUsers().Contains(user))
-                return false;
-            if (!GetCommentsByUser(user).Contains(new Comment{Userid=user.Userid, Midex=midex}))
+            if (_user.GetUserById(user.Userid) == null)
                 return false;
             _comment.DeleteComment(user.Userid, midex);
             return true;
@@ -43,7 +41,7 @@ namespace MuseumBackend.Models
         }
         public static bool DeleteAllCommentByUser(User user)
         {
-            if (!_user.GetAllUsers().Contains(user))
+            if (_user.GetUserById(user.Userid) == null)
                 return false;
             IEnumerable<Comment> comments = GetCommentsByUser(user);
             foreach(var comment in comments)
@@ -53,11 +51,12 @@ namespace MuseumBackend.Models
         }
         public static bool ChangePassword(User user)
         {
-            if (!_user.GetAllUsers().Contains(new User { Userid = user.Userid }) )
+            if (_user.GetUserById(user.Userid) == null)
                 return false;
             User to_change = _user.GetUserById(user.Userid);
             _context.Remove(to_change);
-            _context.Add(user);
+            to_change.Userpwd = user.Userpwd;
+            _context.Add(to_change);
             _context.SaveChanges();
             return true;
         }
